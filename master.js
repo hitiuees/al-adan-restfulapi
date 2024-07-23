9fe170ee83fa0960869a2ddc014cfc48c0b21ee1
hitiuees <ghbadr132800@gmail.com>
2024-07-23 23:41:17 +0100

    finish project

--- master.js
+++ master.js
@@ -1,29 +1,67 @@
+//declare variable needed in the whole code
+let button = document.getElementById("button");
+let containerprayer = document.getElementById("divtime");
 
-let inputcountry=document.querySelectorAll('input')[0].value;
-let  button=document.getElementById("button")
+//function of getting the api by using axios and filtering data by date class
+function getprayertime(inputCity, inputcountry) {
+    let prayertime = new Date();
+    let day = prayertime.getDate() - 1;
+    let month = prayertime.getMonth() + 1;
+    let year = prayertime.getFullYear();
 
+    axios.get(`http://api.aladhan.com/v1/calendarByCity/${year}/${month}?city=${inputCity}&country=${inputcountry}&method=2`)
+        .then((response) => {
+            let datas = response.data.data;
+            
+            if (!datas || datas.length === 0) {
+                alert(`${inputCity} or ${inputcountry} does not exist`);
+                return;
+            }
 
-    button.onclick=function(){  
-        
-        let inputCity = document.querySelectorAll('input')[1].value;
-        console.log(inputCity)}
-      
-  
-
+            let prayerDiv = document.createElement('div');
+            prayerDiv.className = 'icon-container';
+            prayerDiv.innerHTML = `<div>
+                <i class="fas fa-map-marker-alt"></i>
+                <p>${inputCity}</p>
+            </div>
     
+            <div>
+                <i class="fas fa-sun"></i>
+                <p>${datas[day].timings.Fajr}</p>
+            </div>
+            <div>
+                <i class="fas fa-cloud-sun"></i>
+                <p>${datas[day].timings.Dhuhr}</p>
+            </div>
+            <div>
+                <i class="fas fa-cloud-moon"></i>
+                <p>${datas[day].timings.Asr}</p>
+            </div>
+            <div>
+                <i class="fas fa-moon"></i>
+                <p>${datas[day].timings.Maghrib}</p>
+            </div>
+            <div>
+                <i class="fas fa-star"></i>
+                <p>${datas[day].timings.Isha}</p>
+            </div>`;
 
-function getprayertime(){
-    axios.get('http://api.aladhan.com/v1/calendarByCity/2024/7?city=fes&country=morocco&method=2')
-  .then((response)=>{
-    let datas=response.data.data
-  for(data of datas)
-    console.log(data)
-
-
-  })
-  
+            containerprayer.appendChild(prayerDiv);
+        })
+        .catch((error) => {
+            alert(`${inputCity} or ${inputcountry} does not exist`);
+        });
 }
-getprayertime()
 
-
+//function of showing prayer timing at screen 
+button.onclick = function () {
+    containerprayer.innerHTML = '';
+    let inputCity = document.querySelectorAll('input')[1].value;
+    let inputcountry = document.querySelectorAll('input')[0].value;
+    if (inputCity && inputcountry) {
+        getprayertime(inputCity, inputcountry);
+    } else {
+        containerprayer.innerHTML = '';
+    }
+};
 
